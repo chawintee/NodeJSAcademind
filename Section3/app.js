063cfd1da29c02d462f9064d3dac6b3c036293e3
Chawintee <Chawinteetea@gmail.com>
2022-05-01 18:19:08 +0700

    Parsing Request Bodies

--- Section3/app.js
+++ Section3/app.js
@@ -9,12 +9,21 @@ const server = http.createServer((req,res) => {
     if (url === '/'){
         res.write('<html>');
         res.write('<header><title>My First Page</title></header>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit" name="message">Submit</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit" >Submit</button></form></body>');
         res.write('</html>');
         return res.end();
     }
     if (url === '/message' && method === 'POST'){
-        fs.writeFileSync('message.txt', 'Dummy')
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString()
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
         res.statusCode = 302
         res.setHeader('Location','/')
         return res.end()
