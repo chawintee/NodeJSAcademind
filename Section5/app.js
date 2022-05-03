093b550357e6667bbb445c7203bc8b3cdb66073f
Chawintee <Chawinteetea@gmail.com>
2022-05-03 14:15:47 +0700

    use app.listen() instant http.createServer and server.listen

--- Section5/app.js
+++ Section5/app.js
@@ -1,5 +1,3 @@
-const http = require('http')
-
 const express = require('express')
 
 const app = express()
@@ -15,7 +13,4 @@ app.use((req,res,next) => {
     // ...
 })
 
-
-const server = http.createServer(app)
-
-server.listen(3000)
+app.listen(3000)
