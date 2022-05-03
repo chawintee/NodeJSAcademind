61d7c616ef8355ec26114f6f65d944c5f89ec4a8
Chawintee <Chawinteetea@gmail.com>
2022-05-03 21:00:28 +0700

    __dirname .. to rootDir bypath path.dirname(require.main.filename)

--- Section5/routes/admin.js
+++ Section5/routes/admin.js
@@ -1,11 +1,13 @@
 const path = require('path')
 
+const rootDir = require('../util/path')
+
 const express = require('express')
 const router = express.Router()
 
 // /admin/add-product => GET
 router.get('/add-product', (req,res,next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
 // /admin/add-product => POST
