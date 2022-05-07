c6d8cf50415faf216a5f0400b1b421a3a12e0941
Chawintee <Chawinteetea@gmail.com>
2022-05-07 19:03:07 +0700

    add Section6

---
+++ Section6/app.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const adminRouter = require('./routes/admin')
+const shopRouter = require('./routes/shop')
+const { request } = require('express')
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin',adminRouter)
+app.use(shopRouter)
+
+app.use((req,res,next) => {
+    res.sendFile(path.join(__dirname, 'views', 'not-found.html'))
+})
+
+app.listen(3000)
