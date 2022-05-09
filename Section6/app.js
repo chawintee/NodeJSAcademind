085d430a05c4b56483b3ef73ea173d3d706e19c2
Chawintee <Chawinteetea@gmail.com>
2022-05-09 16:14:33 +0700

    Converting our Project to Handlebars

--- Section6/app.js
+++ Section6/app.js
@@ -4,7 +4,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const expressHbs = require('express-handlebars')
 
-
 const app = express()
 
 app.engine('hbs', expressHbs())
