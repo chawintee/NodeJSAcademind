f49d3d343bf3ebe9cdbc6a5e9c2d73458a095eb2
Chawintee <Chawinteetea@gmail.com>
2022-05-12 02:55:59 +0700

    Fetching Data from Files Via the Model

--- Section7/controllers/products.js
+++ Section7/controllers/products.js
@@ -11,8 +11,9 @@ const postAddProduct = (req,res,next) => {
 }
 
 const  getProducts = (req,res,next) => {
-    const products = Product.fetchAll()
-    res.render('shop', {prods: products, pageTitle: "Shop", path: "/", hasProducts: products.length > 0, activeShop: true})
+    Product.fetchAll((products)=>{
+        res.render('shop', {prods: products, pageTitle: "Shop", path: "/", hasProducts: products.length > 0, activeShop: true})
+    })
     // ...
 }
 
--- Section7/models/product.js
+++ Section7/models/product.js
@@ -20,13 +20,13 @@ module.exports = class Product {
         })
     }
 
-    static fetchAll() {
+    static fetchAll(cb) {
         const p = path.join(path.dirname(require.main.filename), "data", "products.json")
         fs.readFile(p, (err,fileContent)=>{
             if(err){
-                return []
+                cb([]) 
             }
-            return JSON.parse(fileContent)
+                cb(JSON.parse(fileContent))
         })
     }
 }
