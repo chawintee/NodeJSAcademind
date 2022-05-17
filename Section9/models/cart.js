cd70b4316d8ecb1ba658f0e9568866c07d3c6af8
Chawintee <Chawinteetea@gmail.com>
2022-05-17 07:48:08 +0700

    Deleting Cart Items

--- Section9/models/cart.js
+++ Section9/models/cart.js
@@ -32,4 +32,23 @@ module.exports = class Cart {
         })
         // Add new product/ increase quantity
     }
+
+    static deleteProduct(id, productPrice){
+        fs.readFile(p, (err,fileContent)=> {
+            if(err){
+                return;
+            }
+            const cart = JSON.parse(fileContent)
+            const updatedCart = {...cart};
+            const product = updatedCart.products.find(prod => prod.id === id)
+            const productQty = product.qty
+            updatedCart.products = updatedCart.products.filter(prod => prod.id !== id)
+            updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty
+            fs.writeFile(p, JSON.stringify(updatedCart), err => {
+                if(!err){
+                    console.log(null);
+                }
+            })
+        })
+    }
 }
--- Section9/models/product.js
+++ Section9/models/product.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 const p = path.join(path.dirname(require.main.filename), "data", "products.json")
 
+const Cart = require('./cart')
+
 const getProductsFromFile = cb => {
         fs.readFile(p, (err,fileContent)=>{
             if(err){
@@ -45,10 +47,12 @@ module.exports = class Product {
 
     static deleteById(id){
         getProductsFromFile(products => {
+            const product = products.find(prod => prod.id === id)
             const updateProducts = products.filter(prod => prod.id !== id)
             fs.writeFile(p,JSON.stringify(updateProducts),err => {
                 if(!err){
                     console.log(null);
+                    Cart.deleteProduct(id, product.price)
                 }
             })
         })
