c7a3f947a2598021c0b8056aa90eefb3b566be57
Chawintee <Chawinteetea@gmail.com>
2022-05-19 22:17:18 +0700

    Fetching Products

--- Section10/app.js
+++ Section10/app.js
@@ -14,14 +14,6 @@ const errorController = require("./controllers/error");
 
 const db = require("./util/database");
 
-db.execute("SELECT * FROM products")
-  .then((result) => {
-    console.log(result[0], result[1]);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
--- Section10/models/product.js
+++ Section10/models/product.js
@@ -1,19 +1,5 @@
-const fs = require('fs')
-const path = require('path')
-
-const p = path.join(path.dirname(require.main.filename), "data", "products.json")
-
+const db = require('../util/database')
 const Cart = require('./cart')
-
-const getProductsFromFile = cb => {
-        fs.readFile(p, (err,fileContent)=>{
-            if(err){
-                cb([]) 
-            }else{
-                cb(JSON.parse(fileContent))
-            }
-        })
-}
 
 module.exports = class Product {
     constructor(id,title, imageUrl, description, price) {
@@ -25,48 +11,18 @@ module.exports = class Product {
     }
 
     save() {
-        if(this.id){
-            getProductsFromFile(products => {
-                const exitingProductIndex = products.findIndex(prod => prod.id === this.id)
-                const updatedProducts = [...products]
-                updatedProducts[exitingProductIndex] = this
-                fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-                    console.log(err);
-                })
-            })
-        }else{
-            this.id = Math.random().toString()
-            getProductsFromFile(products => {
-                products.push(this);
-                fs.writeFile(p, JSON.stringify(products), (err) => {
-                    console.log(err);
-                })
-            })
-        }
+
     }
 
     static deleteById(id){
-        getProductsFromFile(products => {
-            const product = products.find(prod => prod.id === id)
-            const updateProducts = products.filter(prod => prod.id !== id)
-            fs.writeFile(p,JSON.stringify(updateProducts),err => {
-                if(!err){
-                    console.log(null);
-                    Cart.deleteProduct(id, product.price)
-                }
-            })
-        })
+ 
     }
 
     static fetchAll(cb) {
-        getProductsFromFile(cb)
+        return db.execute('SELECT * FROM products')
     }
 
     static findById(id, cb) {
-        getProductsFromFile(products => {
-            // console.log({products});
-            const product = products.find(p => p.id === id)
-            cb(product)
-        })
+  
     }
 }
