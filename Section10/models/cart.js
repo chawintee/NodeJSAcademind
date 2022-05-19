b76e2aea9ff80c5c722de638a4363b714707582f
Chawintee <Chawinteetea@gmail.com>
2022-05-19 20:19:08 +0700

    copy section9 to section10

---
+++ Section10/models/cart.js
@@ -0,0 +1,74 @@
+const fs = require('fs')
+const path = require('path')
+
+const p = path.join(path.dirname(require.main.filename), "data", "cart.json")
+
+
+module.exports = class Cart {
+    static addProduct(id, productPrice) {
+        // Fetch the previous cart
+        fs.readFile(p, (err, fileContent)=> {
+            let cart = {products: [], totalPrice: 0}
+            if(!err){
+                cart = JSON.parse(fileContent);
+            }
+            // Analyze the cart => Find existing product
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id)
+            const existingProduct = cart.products[existingProductIndex]
+            let updatedProduct;
+            if (existingProduct){
+                updatedProduct = {...existingProduct};
+                updatedProduct.qty = existingProduct.qty + 1;
+                cart.products = [...cart.products]
+                cart.products[existingProductIndex] = updatedProduct;
+            }else {
+                updatedProduct = {id: id , qty: 1}
+                cart.products = [...cart.products, updatedProduct]
+            }
+            cart.totalPrice = cart.totalPrice + +productPrice;
+            fs.writeFile(p,JSON.stringify(cart),(err)=>{
+                console.log(err);
+            })
+        })
+        // Add new product/ increase quantity
+    }
+
+    static deleteProduct(id, productPrice){
+        fs.readFile(p, (err,fileContent)=> {
+            if(err){
+                return;
+            }
+            const cart = JSON.parse(fileContent)
+            const updatedCart = {...cart};
+            const product = updatedCart.products.find(prod => prod.id === id)
+            if(!product){
+                return;
+            }
+            const productQty = product.qty
+            updatedCart.products = updatedCart.products.filter(prod => prod.id !== id)
+            updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty
+            fs.writeFile(p, JSON.stringify(updatedCart), err => {
+                if(!err){
+                    console.log(null);
+                }
+            })
+        })
+    }
+
+    static getCart(cb){
+        fs.readFile(p, (err, fileContent) => {
+            if(err){
+                cb(null)
+            }else {
+                if(fileContent.length == 0){
+                    cb(null)
+                }else{
+                    const cart = JSON.parse(fileContent)
+                    cb(cart)
+                }
+            }
+        })
+    }
+
+
+}
---
+++ Section10/routes/shop.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+const shopControllers = require('../controllers/shop')
+
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', shopControllers.getIndex)
+
+router.get('/products', shopControllers.getProducts)
+
+router.get('/product/:productId',shopControllers.getProduct)
+
+router.get('/cart', shopControllers.getCart)
+
+router.post('/cart', shopControllers.postCart)
+
+router.post('/cart-delete-item', shopControllers.postCartDeleteProduct)
+
+router.get('/orders', shopControllers.getOrders)
+
+router.get('/checkout', shopControllers.getCheckout)
+
+module.exports = router
