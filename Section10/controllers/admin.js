490d02dfb02ad3ff4cbe3cf8f1e968fe4db96a51
Chawintee <Chawinteetea@gmail.com>
2022-05-21 20:44:49 +0700

    Deleting Products

--- Section10/controllers/admin.js
+++ Section10/controllers/admin.js
@@ -70,8 +70,20 @@ exports.postEditProduct = (req,res,next) => {
 
 exports.postDeleteProduct = (req,res, next) => {
     const prodId = req.body.productId
-    Product.deleteById(prodId)
-    res.redirect('/admin/products')
+    Product.findByPk(prodId)
+    .then(product => {
+        // console.log(product);
+        return product.destroy()
+    }).then(result => {
+        // console.log(result);
+        console.log("DELETED PRODUCT!!!");
+        res.redirect('/admin/products')
+    })
+    .catch(err => {
+        console.log(err);
+    })
+    // Product.deleteById(prodId)
+    // res.redirect('/admin/products')
 }
 
 
