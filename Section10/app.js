fa394af414b6562d41a825ff3077de13266eff05
Chawintee <Chawinteetea@gmail.com>
2022-05-21 14:44:22 +0700

    Syncing JS Definitions to the Database

--- Section10/app.js
+++ Section10/app.js
@@ -12,7 +12,7 @@ const adminData = require("./routes/admin");
 const shopRouter = require("./routes/shop");
 const errorController = require("./controllers/error");
 
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -22,4 +22,12 @@ app.use(shopRouter);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+
+sequelize.sync()
+.then(result => {
+    // console.log(result);
+    app.listen(3000);
+})
+.catch(err => {
+    console.log(err);
+})
