dfb59f7aefdba05898e691d5e4c14d64e8357687
Chawintee <Chawinteetea@gmail.com>
2022-05-25 20:25:41 +0700

    One-To-Many & Many-To-Many Relations

--- Section10/app.js
+++ Section10/app.js
@@ -15,7 +15,9 @@ const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
 
 const Product = require('./models/product');
-const User = require('./models/user')
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -39,6 +41,11 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, {cosntraints: true, onDelete: 'CASCADE'})
 User.hasMany(Product)
+User.hasOne(Cart)
+Cart.belongsTo(User)
+Cart.belongsToMany(Product, { through: CartItem })
+Product.belongsToMany(Cart, { through: CartItem })
+
 
 sequelize
 // .sync({force: true})
