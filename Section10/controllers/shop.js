65948eb7a924ef47a20df04a1b8cf257fdcfcec0
Chawintee <Chawinteetea@gmail.com>
2022-05-30 23:37:23 +0700

    Storing Cartitems as Orderitems

--- Section10/controllers/shop.js
+++ Section10/controllers/shop.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product')
 const Cart = require('../models/cart')
+const Order = require('../models/order')
 
 exports.getProducts = (req,res,next) => {
     Product.findAll()
@@ -116,6 +117,28 @@ exports.postCartDeleteProduct = (req,res,next) => {
             res.redirect('/cart')
         })
         .catch(err => console.log(err))
+}
+
+exports.postOrder = (req,res,next) => {
+    req.user.getCart()
+    .then(cart => {
+        return cart.getProducts();
+    })
+    .then(products => {
+        // console.log(products);
+        return req.user.createOrder()
+        .then(order => {
+            return order.addProducts(products.map(product => {
+                product.orderItem = {quantity : product.cartItem.quantity}
+                return product;
+            }))
+        })
+         .catch(err => console.log(err))
+    })
+    .then(result => {
+        res.redirect('/orders')
+    })
+    .catch(err => console.log(err))
 }
 
 exports.getOrders = (req,res,next) => {
--- Section10/routes/shop.js
+++ Section10/routes/shop.js
@@ -18,6 +18,8 @@ router.post('/cart', shopControllers.postCart)
 
 router.post('/cart-delete-item', shopControllers.postCartDeleteProduct)
 
+router.post('/create-order',shopControllers.postOrder)
+
 router.get('/orders', shopControllers.getOrders)
 
 router.get('/checkout', shopControllers.getCheckout)
