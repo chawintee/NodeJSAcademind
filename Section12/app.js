04df57cacec7494ae8632fdf05ef0a63ec7c9371
Chawintee <Chawinteetea@gmail.com>
2022-06-05 22:41:36 +0700

    Installing the MongoDB Driver

--- Section12/app.js
+++ Section12/app.js
@@ -8,69 +8,23 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
-const shopRouter = require("./routes/shop");
+const mongoConnect = require('./util/database')
+
+
+// const adminData = require("./routes/admin");
+// const shopRouter = require("./routes/shop");
 const errorController = require("./controllers/error");
-
-const sequelize = require("./util/database");
-
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req,res,next) => {
-    User.findByPk(1)
-    .then(user=>{
-        req.user = user;
-        next()
-    })
-    .catch(err => {
-        console.log(err)
-    })
-})
-
-app.use("/admin", adminData);
-app.use(shopRouter);
+// app.use("/admin", adminData);
+// app.use(shopRouter);
 
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, {cosntraints: true, onDelete: 'CASCADE'})
-User.hasMany(Product)
-User.hasOne(Cart)
-Cart.belongsTo(User)
-Cart.belongsToMany(Product, { through: CartItem })
-Product.belongsToMany(Cart, { through: CartItem })
-Order.belongsTo(User)
-User.hasMany(Order)
-Order.belongsToMany(Product, { through: OrderItem })
-
-
-
-sequelize
-// .sync({force: true})
-.sync()
-.then(result => {
-    return User.findByPk(1)
-}).then( user => {
-    if(!user){
-        return User.create({name:"Test", email: "test@test.com"})
-    }
-    return user
-}).then(user => {
-    // console.log(user);
-    return user.createCart()
-}).then(cart => {
-    // console.log(cart);
+mongoConnect(client => {
+    console.log(client);
     app.listen(3000);
-})
-.catch(err => {
-    console.log(err);
 })
--- Section12/util/database.js
+++ Section12/util/database.js
@@ -1,11 +1,13 @@
-const path = require('path')
-const dotenv = require('dotenv')
-dotenv.config({path: path.join(__dirname, "..", `.env.${process.env.NODE_ENV}`)})
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb')
+const mongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', `${process.env.DATABASE_PASSWORD}`, {
-    dialect: 'mysql',
-    host: 'localhost'
-})
+const mongoConnect = (callback) => {
+    mongoClient.connect('mongodb://root:123456@localhost:27017/?authSource=admin')
+    .then(client => {
+        console.log('Connected !!');
+        return callback(client)
+    })
+    .catch(err => console.error(err))
+}
 
-module.exports = sequelize
+module.exports = mongoConnect
