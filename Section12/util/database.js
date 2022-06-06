6ad4019c00d3e8ac68f36c5edabdc96626fc64a8
Chawintee <Chawinteetea@gmail.com>
2022-06-06 07:32:32 +0700

    Finishing the Database Connection

--- Section12/util/database.js
+++ Section12/util/database.js
@@ -1,13 +1,26 @@
 const mongodb = require('mongodb')
 const mongoClient = mongodb.MongoClient;
 
+let _db
+
 const mongoConnect = (callback) => {
     mongoClient.connect('mongodb://root:123456@localhost:27017/?authSource=admin')
     .then(client => {
+        _db = client.db()
         console.log('Connected !!');
         return callback(client)
     })
-    .catch(err => console.error(err))
+    .catch(err => {
+        console.error(err)
+        throw err
+    })
 }
 
-module.exports = mongoConnect
+const getDb = () => {
+    if(_db){
+        return _db
+    }
+    throw "No datebase found!!"
+}
+
+module.exports = {mongoConnect, getDb }
