468ca0d2b7f133a8745534a1ca159f29b00e54cd
Chawintee <Chawinteetea@gmail.com>
2022-06-08 21:42:23 +0700

    Working on Cart Items & Orders

--- Section12/models/user.js
+++ Section12/models/user.js
@@ -3,15 +3,27 @@ const mongodb = require('mongodb')
 const ObjectId = mongodb.ObjectId
 
 class User {
-    constructor(username, email){
+    constructor(username, email, cart,id){
         this.name = username,
-        this.email = email
+        this.email = email, 
+        this.cart = cart, // {item: []}
+        this._id = id
     }
 
     save(){
         const db = getDb();
         return db.collection('users').insertOne(this)
+    }
 
+    addToCart(product){
+        // const cartProduct = this.cart.item.findIndex(cp => {
+        //     return cp._id === product._id
+        // })
+
+        const db = getDb();
+        const updatedCart = {item :[{...product, quantity: 1}]}
+        return db.collection('users').updateOne({_id: ObjectId(this.id)}, {$set:{updatedCart}})
+        
     }
 
     static findById(userId){
