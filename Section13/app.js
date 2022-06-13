110170681925768a4596dbed2a0a2e063bcbdb6c
Chawintee <Chawinteetea@gmail.com>
2022-06-13 08:19:33 +0700

    Connecting to the MongoDB Server with Mongoose

--- Section13/app.js
+++ Section13/app.js
@@ -47,7 +47,11 @@ app.use(errorController.get404);
 const dotenv = require('dotenv')
 dotenv.config({path: path.join(__dirname, `.env.${process.env.NODE_ENV}` )})
 
-mongoose.connect(`mongodb://root:123456@localhost:27017/?authSource=admin`).then( result => {
+mongoose.connect(`mongodb://root:123456@localhost:27017/?authSource=admin`)
+.then( result => {
     console.log("mongoDB connected");
     app.listen(3000)
+})
+.catch(err => {
+    console.log(err);
 })
