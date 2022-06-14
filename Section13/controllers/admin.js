b4bd84b73bce758eb10ead22f90d15b2444fb982
Chawintee <Chawinteetea@gmail.com>
2022-06-14 22:41:47 +0700

    Deleting Products

--- Section13/controllers/admin.js
+++ Section13/controllers/admin.js
@@ -82,7 +82,7 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndDelete(prodId)
     .then(() => {
       // console.log(result);
       console.log("DELETED PRODUCT!!!");
