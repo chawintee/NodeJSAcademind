2642abb8587c9b173b8f7daccc821fa4613ef0bb
Chawintee <Chawinteetea@gmail.com>
2022-06-16 23:09:45 +0700

    Storing All Order Related Data

--- Section13/controllers/shop.js
+++ Section13/controllers/shop.js
@@ -130,7 +130,7 @@ exports.postOrder = (req,res,next) => {
     // .execPopulate()
     .then(user => {
         const products = user.cart.items.map(i => {
-            return {quantity: i.quantity,product: i.productId }
+            return {quantity: i.quantity,product: {...i.productId._doc} }
         })
         console.log({products});
         const order = new Order({
