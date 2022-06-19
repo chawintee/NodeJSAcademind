edc1f01a8fb19de11a3c4c9b76040b4e4e4be551
Chawintee <Chawinteetea@gmail.com>
2022-06-19 21:41:23 +0700

    Setting a Cookie

--- Section14/controllers/auth.js
+++ Section14/controllers/auth.js
@@ -1,12 +1,13 @@
 exports.getLogin = (req,res,next) => {
+    const isLoggedIn = req.get('Cookie').split('=')[1] === 'true'
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login', 
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: isLoggedIn
     })
 }
 
 exports.postLogin = (req,res,next) => {
-    req.isLoggedIn = true;
+    res.setHeader('set-cookie','loggedIn=true')
     res.redirect('/')
 }
--- Section14/controllers/shop.js
+++ Section14/controllers/shop.js
@@ -156,7 +156,7 @@ exports.postOrder = (req,res,next) => {
 exports.getOrders = (req,res,next) => {
     Order.find({'user.userId': req.user})
     .then(orders => {
-        console.log({orders});
+        // console.log({orders});
         res.render('shop/orders',{
             path: '/orders',
             pageTitle: 'Your Orders',
