5f377008ff085f5f2f00b2f871b24133dbffabfd
Chawintee <Chawinteetea@gmail.com>
2022-06-20 07:42:01 +0700

    Initializing the Session Middleware

--- Section14/app.js
+++ Section14/app.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const session = require("express-session")
 
 const app = express();
 
@@ -20,6 +21,7 @@ const errorController = require("./controllers/error");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session({secret:"my secret", resave: false, saveUninitialized:false}))
 
 const User = require('./models/user')
 
