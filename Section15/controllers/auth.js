6cb38cf07fa7dbc8b63aff8ed074d4fa712b572f
Chawintee <Chawinteetea@gmail.com>
2022-06-27 21:52:50 +0700

    Encrypting Passwords

--- Section15/controllers/auth.js
+++ Section15/controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+
 const User = require('../models/user')
 
 exports.getLogin = (req,res,next) => {
@@ -45,13 +47,16 @@ exports.postSignup = (req,res,next) => {
             if(userDoc){
                 return res.redirect('/signup')
             }
+            return bcrypt.hash(password,12)
+        }).then(hashPassword => {
             const user = new User({
                 email: email,
-                password: password, 
+                password: hashPassword, 
                 cart : {item : []}
             })
             return user.save()
-        }).then(result => {
+        })
+        .then(result => {
             res.redirect(301,'/login')
         })
         .catch(err => {
