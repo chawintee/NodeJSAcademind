aa33921576856ef652e56795f4d189bdf73991a9
Chawintee <Chawinteetea@gmail.com>
2022-06-28 07:51:32 +0700

    Working on Route Protection

--- Section15/controllers/admin.js
+++ Section15/controllers/admin.js
@@ -3,6 +3,9 @@ const product = require("../models/product");
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
+  if(!req?.session?.user){
+    res.redirect('/login')
+  }
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
