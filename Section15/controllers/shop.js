65c3d78c908362289b1a98a967df56ec591e6591
Chawintee <Chawinteetea@gmail.com>
2022-07-09 13:15:15 +0700

    Fixing the Order Button

--- Section15/controllers/shop.js
+++ Section15/controllers/shop.js
@@ -137,7 +137,7 @@ exports.postOrder = (req,res,next) => {
         console.log({products});
         const order = new Order({
             user: {
-                name : req.user.name,
+                email : req.user.email,
                 userId : req.user._id
             },
             products :products
