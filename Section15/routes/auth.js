c1d6ada3d530151c2a5ff5dde33fc55d0f755caf
Chawintee <Chawinteetea@gmail.com>
2022-07-13 09:43:42 +0700

    Adding Logic to Update the Password

--- Section15/routes/auth.js
+++ Section15/routes/auth.js
@@ -20,7 +20,7 @@ router.post('/reset', authController.postReset)
 
 router.get('/reset/:token', authController.getNewPassword)
 
-
+router.post('/new-password', authController.postNewPassword)
 
 
 module.exports = router
