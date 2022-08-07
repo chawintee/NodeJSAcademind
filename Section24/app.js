ad932711920046990c5358f43679a81428beee34
Chawintee <Chawinteetea@gmail.com>
2022-08-07 16:55:47 +0700

    REST APIs, Clients & CORS Errors

--- Section24/app.js
+++ Section24/app.js
@@ -12,6 +12,13 @@ const feedRoutes = require('./routes/feed.js');
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next()
+})
+
 app.use('/feed', feedRoutes)
 
 app.use('/test', (req, res, next) => {
